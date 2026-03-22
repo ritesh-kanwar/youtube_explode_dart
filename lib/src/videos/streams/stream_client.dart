9d6c8d0d45f006388aedc68854bfe2e5ea1dd5f2
Ritesh kanwar <riteshkanwar0309@gmail.com>
2026-03-22 13:12:09 +0530

    feat: add `YoutubeApiClient.androidVr` to the default client list for stream manifest requests

--- lib/src/videos/streams/stream_client.dart
+++ lib/src/videos/streams/stream_client.dart
@@ -65,7 +65,8 @@ class StreamClient {
         'ytClients cannot be an empty list');
 
     videoId = VideoId.fromString(videoId);
-    final clients = ytClients ?? [YoutubeApiClient.androidSdkless];
+    final clients = ytClients ??
+        [YoutubeApiClient.androidVr, YoutubeApiClient.androidSdkless];
 
     if (_jsChallengeSolver != null && ytClients == null) {
       clients.add(YoutubeApiClient.tv);
